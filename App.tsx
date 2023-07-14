cbc62bd860bafda661a04b1d155e466281814479
RandyXXX <hoangvanthanhdev@gmail.com>
2023-07-15 06:18:33 +0700

    push android ios

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React, {useCallback, useState} from 'react';
+import {SafeAreaView, TouchableOpacity, Text} from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'red'}}></SafeAreaView>
+  );
+};
+
+export default App;
